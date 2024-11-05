e8436e02e47c25c5d0b97955890e604f625449c9
Aaron Wang <aaronwang04@icloud.com>
2024-11-05 15:25:52 -0500

    deployment attempt

--- server/server.js
+++ server/server.js
@@ -12,7 +12,7 @@ app.use(cors());
 
 const io = new Server(server, {
     cors: {
-        origin: "http://localhost:5173", // Replace with your Vite app URL
+        origin: process.env.FRONTEND_URL || "http://localhost:5173",
         methods: ["GET", "POST"],
         allowedHeaders: ["my-custom-header"],
         credentials: true
@@ -187,6 +187,7 @@ io.on('connection', (socket) => {
 
 });
 
-server.listen(3000, () => {
-    console.log('Server is running on port 3000');
+const PORT = process.env.PORT || 3000;
+server.listen(PORT, () => {
+    console.log(`Server is running on port ${PORT}`);
 });
