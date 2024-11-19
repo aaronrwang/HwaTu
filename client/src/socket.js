2a199e11bb893ac1f0af91011250b89064cbe3ba
Aaron Wang <aaronwang04@icloud.com>
2024-11-19 14:04:02 -0500

    test

--- client/src/socket.js
+++ client/src/socket.js
@@ -3,7 +3,7 @@ import { io } from 'socket.io-client';
 
 // const socket = io('https://hwatu.onrender.com'); // Replace with your server URL
 // "http://localhost:5173"
-const socket = io('http://localhost:3000', {
+const socket = io('https://hwatu.onrender.com', {
     transports: ['websocket'],  // This forces WebSocket, disabling fallback to polling
     withCredentials: true  // Ensure credentials (cookies, etc.) are sent
 });
